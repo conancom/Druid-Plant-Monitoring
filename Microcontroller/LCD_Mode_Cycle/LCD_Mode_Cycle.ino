892a54f55f67bcb4f85c947ce18a76f35c7ec916
charnar <charn.arunkit@gmail.com>
2022-04-21 22:02:52 +0700

    Improved the Button to cycle the modes

--- Microcontroller/LCD_Mode_Cycle/LCD_Mode_Cycle.ino
+++ Microcontroller/LCD_Mode_Cycle/LCD_Mode_Cycle.ino
@@ -2,10 +2,13 @@
 #include <TimerOne.h>
 #include <LiquidCrystal_I2C.h>
 
-const byte toggleButton = 2;
+const int buttonPin = 2;                  // Pin of the toggle button
+int lastButtonState;
+int currentButtonState;
+
 const unsigned long duration = 3000000;   // Duration of display for 1 mode
 volatile int modeSelect = 0;              // Index of modes array   
-int modeSelectCopy = 0;                   // Copy of Index of modes array
+int modeSelectCopy;                   // Copy of Index of modes array
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 String modes[5] = {"1 Temperature", "2 Soil Moisture", "3 Air Quality", "4 Humidity", "5 Sunlight"};  // Modes for LCD display
@@ -21,26 +24,16 @@ void setup(void) {
   lcd.begin();
   lcd.backlight();
   displayMode(modeSelect);
-  pinMode(toggleButton, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(toggleButton), manualChange, FALLING);
+  
+  pinMode(buttonPin, INPUT_PULLUP);
+  
   Timer1.initialize(duration);
-  Timer1.attachInterrupt(autoChange);
+  Timer1.attachInterrupt(changeMode);
   
 }
 
-void manualChange(void) {
-  modeSelect = modeSelect + 1;
 
-  if (modeSelect > 4) { // If the pointer is over the length of modes array
-    modeSelect = 0;
-  }
-
-  Timer1.restart();
-  modeSelect = modeSelect - 1;
-}
-
-
-void autoChange(void) { // Timer1 Interrupt (3 Seconds)
+void changeMode(void) { // Timer1 Interrupt (3 Seconds)
   modeSelect = modeSelect + 1;
   
   if (modeSelect > 4) { // If the pointer is over the length of modes array
@@ -49,6 +42,16 @@ void autoChange(void) { // Timer1 Interrupt (3 Seconds)
 }
 
 void loop(void) {
+  
+  lastButtonState = currentButtonState;
+  currentButtonState = digitalRead(buttonPin);
+  
+
+  if (lastButtonState == HIGH && currentButtonState == LOW) {
+    Timer1.restart();
+  }
+
+  
   if (modeSelect != modeSelectCopy) {   // Move to the next mode is the modeSelect does not equal its copy
     displayMode(modeSelect);            // Update the display to the next mode
     modeSelectCopy = modeSelect;        
