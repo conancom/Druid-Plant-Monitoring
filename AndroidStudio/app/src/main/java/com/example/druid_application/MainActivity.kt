1cc252a79fd9ba1ab6fb9258018064f2ee711af0
ConanCoM (Leon) <cnoanplays@gmail.com>
2022-04-22 11:16:46 +0700

    started on bluetooth

--- AndroidStudio/app/src/main/java/com/example/druid_application/MainActivity.kt
+++ AndroidStudio/app/src/main/java/com/example/druid_application/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.druid_application
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.druid_application.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,6 +38,48 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val bAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bAdapter == null) {
+            // Device won't support Bluetooth
+        }
+
+
+        if (!bAdapter.isEnabled) {
+            val eintent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+
+        }
+
+        val dIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+        dIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_ADVERTISE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        startActivity(dIntent)
+
+
+        // Get paired devices.
+        // Get paired devices.
+        val pairedDevices = bAdapter.bondedDevices
+        if (pairedDevices.size > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (device in pairedDevices) {
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,4 +103,7 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+
+
+
 }
